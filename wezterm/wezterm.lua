5c1347129d2c524b00b710b16f578c653e742f83
Sankar Raj <sank.nec@gmail.com>
2024-09-09 12:41:09 +0530

    Added gruvbox and set mononoki font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,8 +2,8 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("Ubuntu Mono")
-config.font_size = 14
+config.font = wezterm.font("mononoki")
+config.font_size = 13
 
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
