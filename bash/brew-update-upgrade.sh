7ebb4870f437eacc6610740033b79bb06fdbd4d8
sksankarraj <sank.nec@gmail.com>
2024-12-26 13:09:55 +0530

    improved lot of dotfiles to improve devEx and productivity

---
+++ bash/brew-update-upgrade.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export PATH=/usr/local/bin:/opt/homebrew/bin:$PATH  # Ensure Homebrew is in the PATH
+brew update
+brew upgrade
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -20,7 +20,7 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
 
 			local lspconfig = require("lspconfig")
