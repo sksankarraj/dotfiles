987dec3532d955eb4e08bcd204707a259a027e13
Sankar Raj <sankar.raj@appfire.com>
2026-04-26 07:09:39 +0530

    zsh and tmux updates

--- bash/brew-update-upgrade.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "" > ~/.config/bash/brew-update-upgrade.log
-export PATH=/usr/local/bin:/opt/homebrew/bin:$PATH  # Ensure Homebrew is in the PATH
-brew update
-brew upgrade
-brew cleanup
